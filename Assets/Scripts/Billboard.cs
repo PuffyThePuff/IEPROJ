ca0571728c54ae49580ba765e2a94271248e9de4
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-22 00:34:22 +0800

    Created Character prefab, imported some assets for testing

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(mainCamera.transform);
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+}
