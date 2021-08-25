72515c514e222dfcaf51476989195ffbf81a9d28
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-25 19:56:40 +0800

    Created Dialogue System

---
+++ Assets/Scripts/Dialogue/NodeParser.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XNode;
+
+public class NodeParser : MonoBehaviour
+{
+    public DialogueGraph graph;
+    Coroutine _parser;
+    public Text speaker;
+    public Text dialogue;
+    public Image speakerImage;
+
+    private void Start()
+    {
+        //finds the start node and make it the starting point
+        foreach (BaseNode b in graph.nodes)
+        {
+            if (b.GetString() == "Start")
+            {
+                graph.current = b;
+                break;
+            }
+        }
+
+        _parser = StartCoroutine(ParseNode());
+    }
+
+    IEnumerator ParseNode()
+    {
+        BaseNode b = graph.current;
+        string data = b.GetString();
+        string[] dataParts = data.Split('/');
+
+        //parse start node
+        if (dataParts[0] == "Start")
+        {
+            NextNode("exit");
+        }
+
+        //check if it actually worked
+        if (dataParts[0] == "DialogueNode")
+        {
+            //run dialogue processing
+            speaker.text = dataParts[1];
+            dialogue.text = dataParts[2];
+            speakerImage.sprite = b.GetSprite();
+
+            //waits for left mouse click before going to next node
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+            yield return new WaitUntil(() => Input.GetMouseButtonUp(1));
+            NextNode("exit");
+        }
+    }
+
+    //find the port with this name
+    public void NextNode(string fieldName)
+    {
+        if (_parser != null)
+        {
+            StopCoroutine(_parser);
+            _parser = null;
+        }
+
+        foreach (NodePort p in graph.current.Ports)
+        {
+            if (p.fieldName == fieldName)
+            {
+                graph.current = p.Connection.node as BaseNode;
+                break;
+            }
+        }
+        _parser = StartCoroutine(ParseNode());
+    }
+}
