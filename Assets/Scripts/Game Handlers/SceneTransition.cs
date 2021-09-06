636058a8fd0511efa5254b2d1a60d5906c2d2ecd
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-09-06 15:07:44 +0800

    Restructured framework for scene transition

--- Assets/Scripts/Game Handlers/SceneTransition.cs
+++ Assets/Scripts/Game Handlers/SceneTransition.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
+    public Vector3 playerPosition;
+    public VectorValue playerStorage;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
+            playerStorage.initialValue = playerPosition;
             SceneManager.LoadScene(sceneToLoad);
         }
     }
