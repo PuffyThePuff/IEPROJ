b9e758ed98b0066be34fdb750e5147b88798c90f
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2022-02-19 18:29:43 +0800

    Readded gacha changes

--- Assets/Scripts/GachaListener.cs
+++ Assets/Scripts/GachaListener.cs
@@ -7,18 +7,28 @@ using UnityEngine.Video;
 //simple listener for gacha events
 public class GachaListener : MonoBehaviour
 {
+    [SerializeField]private Sprite char1;
+
     private VideoPlayer videoPlayer;
+    [SerializeField]private GameObject image;
 
     // Start is called before the first frame update
     void Start()
     {
         GameEvents.current.onGachaSuccess += PlayVid;
         videoPlayer = this.GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += ShowSprite;
     }
 
     private void PlayVid(int rarity)
     {
         videoPlayer.Play();
-        Debug.Log("boop");
+        Debug.Log("done");
+    }
+
+    private void ShowSprite(UnityEngine.Video.VideoPlayer vp)
+    {
+        image.GetComponent<Image>().sprite = char1;
+        image.SetActive(true);
     }
 }
